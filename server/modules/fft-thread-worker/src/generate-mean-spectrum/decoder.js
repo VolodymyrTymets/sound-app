2183f17909180ada9e887ac408fb8a16567c929a
volodymyr <volodymyrtymets@gmail.com>
2018-07-31 13:25:30 +0300

    add calculation mean folder

--- server/modules/fft-thread-worker/src/generate-mean-spectrum/decoder.js
+++ server/modules/fft-thread-worker/src/generate-mean-spectrum/decoder.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 const WavDecoder = require('wav-decoder');
+const { range } = require('lodash');
+const jsonfile = require('jsonfile');
+const path = require('path');
+
+const { fft, spliceSpectrum } = require('../utils/fft');
+const { getSpectrumEnergy } = require('../utils/get-spectrum-energy');
+const { config } = require('../../config');
+
 
 const decode = filePath => {
 	const readFile = filepath => {
@@ -18,4 +26,40 @@ const decode = filePath => {
 	});
 };
 
-module.exports = { decode };
+const decodeSingleIn = async (folderPath, fileName) => {
+	const inFilePath = path.resolve(folderPath,  fileName);
+	const outFilePath = path.resolve(folderPath, '../out', `${fileName}.json`);
+
+	const audioData = await decode(inFilePath);
+
+	const wave = audioData.channelData[0];
+	const { spectrum } = fft(wave);
+	const { splicedSpectrum } = spliceSpectrum(spectrum);
+	const res = [];
+
+	range(config.N, splicedSpectrum.length).forEach(() => res.push({
+		frequency:0, amplitude: 0,
+	}));
+	splicedSpectrum.forEach(x=> res.push(x));
+	const energy = getSpectrumEnergy(spectrum, 10);
+	jsonfile.writeFileSync(outFilePath, { energy, meanSpectrum: res});
+};
+
+
+const readInFolder = (folderPath) => new Promise((resolve, reject) => {
+	const inFolderPath = path.resolve(folderPath, './in');
+	fs.readdir(inFolderPath, async (err, files) => {
+		if (err) reject();
+		for (let i = 0; i < files.length; i++) {
+			try {
+				await decodeSingleIn(inFolderPath, files[i]);
+			} catch (e) {
+				reject(e);
+			}
+		}
+		resolve();
+	});
+});
+
+
+module.exports = { readInFolder };
--- server/modules/fft-thread-worker/src/generate-mean-spectrum/index.js
+++ server/modules/fft-thread-worker/src/generate-mean-spectrum/index.js
@@ -1,33 +1,21 @@
-const { range } = require('lodash');
-const jsonfile = require('jsonfile');
 const path = require('path');
 
-const { decode } = require('./decoder');
-const { fft, spliceSpectrum } = require('../fft');
-const { getSpectrumEnergy } = require('../get-spectrum-energy');
-const { config } = require('../../config');
+const { readInFolder } = require('./decoder');
+const { readOutFolder, clearOutFolder } = require('./calculate-mean');
+const folderName = process.argv[2] || 'nerve';
+const assetsPath = path.resolve(__dirname, './', 'assets', folderName);
 
-const inFilePath = path.resolve(__dirname, './', 'assets/in', '1.wav');
-const outFilePath = path.resolve(__dirname, './', 'assets/out', '1.json');
+const calculate = async() => {
+	try {
+		await clearOutFolder(assetsPath);
+		console.log('--> cleaned')
+		await readInFolder(assetsPath);
+		console.log('--> decoded')
+		await readOutFolder(assetsPath);
+		console.log('--> calculated')
+	} catch (e) {
+		console.log(e);
+	}
+};
 
-decode(inFilePath)
-	.then(audioData => {
-		const wave = audioData.channelData[0];
-		const { spectrum } = fft(wave);
-		const splicedSpectrum = spliceSpectrum(spectrum);
-		const res = [];
-
-		range(config.N, splicedSpectrum.length).forEach(() => res.push({
-			frequency:0, amplitude: 0,
-		}));
-		splicedSpectrum.forEach(x=> res.push(x));
-		const energy = getSpectrumEnergy(spectrum, 10);
-
-		console.log(res.length);
-		jsonfile.writeFile(outFilePath, { energy, meanSpectrum: res} , (err) => {
-			if (err) console.error(err);
-			console.log('spectrum successfully added for:');
-			console.log('--->' , outFilePath);
-		});
-	})
-	.catch(console.log);
+calculate();
